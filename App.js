9588c48b1445f08109d085b5421a0ebd43da7b99
ranyalbqrq <ranyelle2001@gmail.com>
2023-06-03 23:07:39 -0300

    App React-native Cep

--- App.js
+++ App.js
@@ -1,20 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { View, TextInput, Button, Text, Alert } from 'react-native';
 
-export default function App() {
+
+const BuscaCEPApp = () => {
+  const [cep, setCep] = useState('');
+  const [endereco, setEndereco] = useState('');
+
+  const buscarCEP = async () => {
+    try {
+      const response = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
+      const data = await response.json();
+
+      if (response.ok && !data.erro) {
+        setEndereco(`${data.logradouro}, ${data.bairro}, ${data.localidade}, ${data.uf}`);
+      } else {
+        setEndereco('');
+        Alert.alert('Erro', 'CEP não encontrado');
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#87CEEB' }}>
+      <Text>Informe o CEP desejado:</Text>
+      <TextInput
+        style={{ height: 40, width: 200, borderColor: 'gray', borderWidth: 1, margin: 10, }}
+        onChangeText={text => setCep(text)}
+        value={cep}
+        keyboardType="numeric"
+      />
+      <Button title="Buscar" onPress={buscarCEP} />
+      <Text style={{ marginTop: 20 }}>{endereco}</Text>
     </View>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default BuscaCEPApp;
